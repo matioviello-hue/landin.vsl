afae7a58822d5aad64047040b950433e63498127
Matias Oviedo <matioviello@gmail.com>
2026-03-02 16:53:36 -0300

    Update video source to Vimeo link

--- components/video-section.tsx
+++ components/video-section.tsx
@@ -7,7 +7,7 @@ export function VideoSection() {
         <div className="relative aspect-video w-full overflow-hidden rounded-lg border border-border bg-black">
           <video
             className="absolute inset-0 h-full w-full object-cover"
-            src="vsl.mp4"
+            src="https://vimeo.com/1169674089?share=copy&fl=sv&fe=ci"
             autoPlay
             playsInline
             loop
