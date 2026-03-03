679efc506d2288e46f8ebb297a42d81cbf904126
Matias Oviedo <matioviello@gmail.com>
2026-03-03 19:19:16 -0300

    Integrate Facebook Pixel tracking in RootLayout
    Added Facebook Pixel tracking script to the layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,18 +16,40 @@ export const viewport: Viewport = {
   width: 'device-width',
   initialScale: 1,
 }
+import Script from "next/script";
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const PIXEL_ID = "TU_PIXEL_ID";
+
   return (
     <html lang="es">
-      <body className={`${_inter.variable} ${_outfit.variable} font-sans antialiased`}>
+      <body>
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${PIXEL_ID}');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src={`https://www.facebook.com/tr?id=${PIXEL_ID}&ev=PageView&noscript=1`}
+          />
+        </noscript>
+
         {children}
-        <Analytics />
       </body>
     </html>
-  )
+  );
 }
