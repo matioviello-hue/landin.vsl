8689d30186c91de19b3bb04f9c8e9fac452aa0ab
Matias Oviedo <matioviello@gmail.com>
2026-03-03 19:59:05 -0300

    Rename GraciasPage to felicidadesPage

--- app/felicidades/page.tsx
+++ app/felicidades/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect } from "react";
 
-export default function GraciasPage() {
+export default function felicidadesPage() {
 
   useEffect(() => {
     // Dispara evento para Meta Pixel
